1d021d8fe0a6bf9b8bc1565bef2d1b4f18a607f4
Ralph Cox <ralphsmail@btinternet.com>
2023-11-30 23:16:13 +0000

    Supabase Auth

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,12 @@
 import { defineConfig } from 'astro/config';
-
 import preact from "@astrojs/preact";
+
+import netlify from "@astrojs/netlify/functions";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://remarkable-fairy-a9040b.netlify.app/",
-  integrations: [preact()]
+  integrations: [preact()],
+  output: "server",
+  adapter: netlify()
 });
